4a17de07b5e1dbcbffe794a646a857de333d50c5
Richard Kellnberger <git@richardk2n.de>
2024-06-18 15:36:28 +0200

    dual style plotting

--- fluidx3d/eval/style.py
+++ fluidx3d/eval/style.py
@@ -35,6 +35,8 @@ def rgb(r, g, b):
     return r / 255, g / 255, b / 255
 
 
+activeStyle = "light"
+cm = 1 / 2.54
 tabColors = list(mcolors.TABLEAU_COLORS.keys())
 
 
@@ -43,16 +45,66 @@ def color(index):
     return tabColors[index]
 
 
-backgroundColor = rgb(255, 255, 255)
-
-
-def dark(r=51, g=51, b=51):
-    global backgroundColor
-    plt.style.use("dark_background")
-    backgroundColor = rgb(r, g, b)
+def style(style: str, r, g, b):
+    global activeStyle
+    activeStyle = style
     plt.rcParams["figure.facecolor"] = rgb(r, g, b)
     plt.rcParams["axes.facecolor"] = rgb(r, g, b)
     plt.rcParams["savefig.facecolor"] = rgb(r, g, b)
 
 
-cm = 1 / 2.54
+def dark_():
+    style("dark", 51, 51, 51)
+
+
+def dark():
+    plt.style.use("dark_background")
+    dark_()
+
+
+def light_():
+    style("light", 255, 255, 255)
+
+
+def light():
+    plt.style.use("default")
+    light_()
+
+
+def savefig(name: str, **kwargs):
+    global activeStyle
+    if activeStyle == "light":
+        plt.savefig(f"{name}#light.pdf", **kwargs)
+    elif activeStyle == "dark":
+        plt.savefig(f"{name}#dark.pdf", **kwargs)
+    else:
+        plt.savefig(f"{name}.pdf", **kwargs)
+
+
+def styled(plotFunction):
+    with plt.style.context("default"):
+        light_()
+        plotFunction()
+        plt.close()
+    with plt.style.context("dark_background"):
+        dark_()
+        plotFunction()
+        plt.show()
+
+
+def styledS(plotFunction):
+    with plt.style.context("default"):
+        light_()
+        plotFunction()
+        plt.close()
+    with plt.style.context("dark_background"):
+        dark_()
+        plotFunction()
+        plt.close()
+
+
+def figure(**kwargs):
+    return plt.figure(figsize=(15.5 * cm, 15.5 / 2 * cm), **kwargs)
+
+
+dark()
